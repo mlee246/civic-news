b404daf2613b666530986deb61ffdbf8a8c140f0
Meabh <meabh.olenain.cobc@gmail.com>
2024-07-01 13:51:27 +0100

    voting amended

--- src/components/Votes.jsx
+++ src/components/Votes.jsx
@@ -4,52 +4,51 @@ import axios from "axios";
 function Votes({ articleVotes, article_id }) {
   const [votes, setVotes] = useState(articleVotes);
   const [disabled, setDisabled] = useState(false);
+  const [hasVoted, setHasVoted] = useState(false);
   const [err, setErr] = useState(null);
 
-  function handleVote(vote) {
-    setVotes((articleVotes += vote));
+  const handleVote = (vote) => {
+    if (hasVoted) return;
+
+    const newVotes = votes + vote;
+    setVotes(newVotes);
     setDisabled(true);
+
     axios
-      .patch(
-        `https://be-nc-news-zmuo.onrender.com/api/articles/${article_id}`,
-        { inc_votes: vote }
-      )
+      .patch(`https://be-nc-news-zmuo.onrender.com/api/articles/${article_id}`, {
+        inc_votes: vote,
+      })
       .then(() => {
-        setVotes(articleVotes);
         setDisabled(false);
+        setHasVoted(true);
       })
       .catch(() => {
-        setVotes((articleVotes -= vote));
-        setErr(
-          "Your vote wasn't able to be posted, please reload and try again"
-        );
+        setVotes(votes);
+        setErr("Your vote wasn't able to be posted, please reload and try again");
+        setDisabled(false);
       });
-  }
+  };
 
   return (
-        <section className="article-votes">
-        {err ? (<p className="vote-button-error">{err}</p>):(
-
-          <section className="article-votes">
-
-            <p className="article-vote">Vote</p>
-            <button
-              className="view-article-vote-button"
-              onClick={() => handleVote(-1)}
-              disabled={disabled}
-            >ᐁ</button>
-
-            <p>{votes}</p>
-
-            <button
-              className="view-article-vote-button"
-              onClick={() => handleVote(1)}
-              disabled={disabled}
-            >ᐃ</button>
-
-           </section>
-          )}
-          </section>
+    <section className="article-votes">
+      {err && <p className="vote-button-error">{err}</p>}
+      <p className="article-vote">Vote</p>
+      <button
+        className="view-article-vote-button"
+        onClick={() => handleVote(-1)}
+        disabled={disabled || hasVoted}
+      >
+        ᐁ
+      </button>
+      <p>{votes}</p>
+      <button
+        className="view-article-vote-button"
+        onClick={() => handleVote(1)}
+        disabled={disabled || hasVoted}
+      >
+        ᐃ
+      </button>
+    </section>
   );
 }
 
